2042b89042239faf3b75bbd91ba3c60119061005
ashycodes <64503359+ashycodes@users.noreply.github.com>
2024-01-12 17:27:13 -0600

    Wwise Event Update
    Added all potential Wwise events. Added music events.

--- Lasso/GeneratedSoundBanks/wwise_ids.gd
+++ Lasso/GeneratedSoundBanks/wwise_ids.gd
@@ -2,16 +2,46 @@ class_name AK
 
 class EVENTS:
 
-	const SFX_TEST = 3018450665
-	const HORSE_NEIGH = 4152520648
-	const LASSO_CATCH = 1367699361
+	const HORSE_KICK = 3198588461
+	const HOG_SLEEPING = 998140643
+	const HOG_WALK = 1493740447
 	const HORSE_FOOTSTEPS = 701459736
+	const HORSE_NEIGH_BABY_SITS = 3018083661
+	const BABY_TALK_POSITIVE = 3269556576
+	const FRUIT_PLUCK = 336049027
+	const BABY_TALK_THROW = 3342570235
+	const LASSO_THROW = 362990204
+	const HORSE_NEIGH_POSITIVE = 487887902
+	const BABY_FLY = 1288370271
+	const GAMEPLAY_MUSIC = 2322231365
+	const END_MUSIC = 1428057472
+	const LASSO_WHIRL = 2401031616
+	const LASSO_CATCH = 1367699361
+	const HOG_EAT = 2529140474
+	const WHIP_TANSITION = 3944649217
+	const MAIN_MENU_MUSIC = 3399698792
+	const SFX_TEST = 3018450665
 
 	const _dict = {
-		"SFX_TEST": SFX_TEST,
-		"Horse_Neigh": HORSE_NEIGH,
+		"Horse_Kick": HORSE_KICK,
+		"Hog_Sleeping": HOG_SLEEPING,
+		"Hog_Walk": HOG_WALK,
+		"Horse_Footsteps": HORSE_FOOTSTEPS,
+		"Horse_Neigh_Baby_Sits": HORSE_NEIGH_BABY_SITS,
+		"Baby_Talk_Positive": BABY_TALK_POSITIVE,
+		"Fruit_Pluck": FRUIT_PLUCK,
+		"Baby_Talk_Throw": BABY_TALK_THROW,
+		"Lasso_Throw": LASSO_THROW,
+		"Horse_Neigh_Positive": HORSE_NEIGH_POSITIVE,
+		"Baby_Fly": BABY_FLY,
+		"Gameplay_Music": GAMEPLAY_MUSIC,
+		"End_Music": END_MUSIC,
+		"Lasso_Whirl": LASSO_WHIRL,
 		"Lasso_Catch": LASSO_CATCH,
-		"Horse_Footsteps": HORSE_FOOTSTEPS
+		"Hog_Eat": HOG_EAT,
+		"Whip_Tansition": WHIP_TANSITION,
+		"Main_Menu_Music": MAIN_MENU_MUSIC,
+		"SFX_TEST": SFX_TEST
 	}
 
 class STATES:
