138eadb9598649d82bc0049202643078511896ec
ashycodes <64503359+ashycodes@users.noreply.github.com>
2024-01-13 12:59:51 -0600

    Added Several Events to Wwise
    button activated
    button deactivated
    sign on
    sign off
    rock moving
    dynamite activated
    dynamite explosion
    horse jump
    hog sleeping
    hog walk
    hog eat

--- Lasso/GeneratedSoundBanks/wwise_ids.gd
+++ Lasso/GeneratedSoundBanks/wwise_ids.gd
@@ -2,46 +2,64 @@ class_name AK
 
 class EVENTS:
 
-	const HORSE_KICK = 3198588461
-	const HOG_SLEEPING = 998140643
-	const HOG_WALK = 1493740447
-	const HORSE_FOOTSTEPS = 701459736
-	const HORSE_NEIGH_BABY_SITS = 3018083661
-	const BABY_TALK_POSITIVE = 3269556576
-	const FRUIT_PLUCK = 336049027
-	const BABY_TALK_THROW = 3342570235
-	const LASSO_THROW = 362990204
-	const HORSE_NEIGH_POSITIVE = 487887902
-	const BABY_FLY = 1288370271
-	const GAMEPLAY_MUSIC = 2322231365
-	const END_MUSIC = 1428057472
-	const LASSO_WHIRL = 2401031616
 	const LASSO_CATCH = 1367699361
 	const HOG_EAT = 2529140474
-	const WHIP_TANSITION = 3944649217
-	const MAIN_MENU_MUSIC = 3399698792
+	const HOG_WALK = 1493740447
+	const BABY_TALK_THROW = 3342570235
+	const LASSO_THROW = 362990204
+	const END_MUSIC = 1428057472
+	const LEVEL_FINISH = 4278251541
+	const FRUIT_PLUCK = 336049027
 	const SFX_TEST = 3018450665
+	const HORSE_FOOTSTEPS = 701459736
+	const BABY_FLY = 1288370271
+	const BABY_TALK_POSITIVE = 3269556576
+	const HORSE_NEIGH_POSITIVE = 487887902
+	const WHIP_TANSITION = 3944649217
+	const HOG_SLEEPING = 998140643
+	const GAMEPLAY_MUSIC = 2322231365
+	const HORSE_NEIGH_BABY_SITS = 3018083661
+	const MAIN_MENU_MUSIC = 3399698792
+	const LASSO_WHIRL = 2401031616
+	const HORSE_KICK = 3198588461
+	const SIGN_ON = 1295442366
+	const DYNAMITE_ACTIVATED = 704518912
+	const SIGN_OFF = 1130713572
+	const BUTTON_ACTIVATED = 3746142581
+	const DYNAMITE_EXPLOSION = 1512346300
+	const ROCK_MOVING = 2491012383
+	const BUTTON_DEACTIVATED = 3236117730
+	const HORSE_JUMP = 1466783479
 
 	const _dict = {
-		"Horse_Kick": HORSE_KICK,
-		"Hog_Sleeping": HOG_SLEEPING,
-		"Hog_Walk": HOG_WALK,
-		"Horse_Footsteps": HORSE_FOOTSTEPS,
-		"Horse_Neigh_Baby_Sits": HORSE_NEIGH_BABY_SITS,
-		"Baby_Talk_Positive": BABY_TALK_POSITIVE,
-		"Fruit_Pluck": FRUIT_PLUCK,
-		"Baby_Talk_Throw": BABY_TALK_THROW,
-		"Lasso_Throw": LASSO_THROW,
-		"Horse_Neigh_Positive": HORSE_NEIGH_POSITIVE,
-		"Baby_Fly": BABY_FLY,
-		"Gameplay_Music": GAMEPLAY_MUSIC,
-		"End_Music": END_MUSIC,
-		"Lasso_Whirl": LASSO_WHIRL,
 		"Lasso_Catch": LASSO_CATCH,
 		"Hog_Eat": HOG_EAT,
+		"Hog_Walk": HOG_WALK,
+		"Baby_Talk_Throw": BABY_TALK_THROW,
+		"Lasso_Throw": LASSO_THROW,
+		"End_Music": END_MUSIC,
+		"Level_Finish": LEVEL_FINISH,
+		"Fruit_Pluck": FRUIT_PLUCK,
+		"SFX_TEST": SFX_TEST,
+		"Horse_Footsteps": HORSE_FOOTSTEPS,
+		"Baby_Fly": BABY_FLY,
+		"Baby_Talk_Positive": BABY_TALK_POSITIVE,
+		"Horse_Neigh_Positive": HORSE_NEIGH_POSITIVE,
 		"Whip_Tansition": WHIP_TANSITION,
+		"Hog_Sleeping": HOG_SLEEPING,
+		"Gameplay_Music": GAMEPLAY_MUSIC,
+		"Horse_Neigh_Baby_Sits": HORSE_NEIGH_BABY_SITS,
 		"Main_Menu_Music": MAIN_MENU_MUSIC,
-		"SFX_TEST": SFX_TEST
+		"Lasso_Whirl": LASSO_WHIRL,
+		"Horse_Kick": HORSE_KICK,
+		"Sign_On": SIGN_ON,
+		"Dynamite_Activated": DYNAMITE_ACTIVATED,
+		"Sign_Off": SIGN_OFF,
+		"Button_Activated": BUTTON_ACTIVATED,
+		"Dynamite_Explosion": DYNAMITE_EXPLOSION,
+		"Rock_Moving": ROCK_MOVING,
+		"Button_Deactivated": BUTTON_DEACTIVATED,
+		"Horse_Jump": HORSE_JUMP
 	}
 
 class STATES:
@@ -84,12 +102,12 @@ class AUX_BUSSES:
 
 class AUDIO_DEVICES:
 
-	const SYSTEM = 3859886410
 	const NO_OUTPUT = 2317455096
+	const SYSTEM = 3859886410
 
 	const _dict = {
-		"System": SYSTEM,
-		"No_Output": NO_OUTPUT
+		"No_Output": NO_OUTPUT,
+		"System": SYSTEM
 	}
 
 class EXTERNAL_SOURCES:
