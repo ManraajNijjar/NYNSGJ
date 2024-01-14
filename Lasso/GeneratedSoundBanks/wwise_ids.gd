1fbce7f6735856fa9f4957e98cdafc1c1228263d
ashycodes <64503359+ashycodes@users.noreply.github.com>
2024-01-13 22:35:43 -0600

    Added the last SFX and mixed SFX

--- Lasso/GeneratedSoundBanks/wwise_ids.gd
+++ Lasso/GeneratedSoundBanks/wwise_ids.gd
@@ -2,68 +2,68 @@ class_name AK
 
 class EVENTS:
 
-	const LASSO_CATCH = 1367699361
-	const LASSO_THROW = 362990204
-	const BABY_FLY = 1288370271
-	const LASSO_WHIRL = 2401031616
-	const HOG_WALK = 1493740447
-	const HORSE_FOOTSTEPS = 701459736
-	const WHIP_TANSITION = 3944649217
-	const BABY_TALK_THROW = 3342570235
-	const FRUIT_PLUCK = 336049027
-	const BUTTON_ACTIVATED = 3746142581
-	const ROCK_MOVING = 2491012383
-	const HORSE_KICK = 3198588461
+	const HORSE_NEIGH_BABY_SITS = 3018083661
 	const BABY_TALK_POSITIVE = 3269556576
 	const HOG_EAT = 2529140474
-	const HORSE_NEIGH_POSITIVE = 487887902
-	const LEVEL_FINISH = 4278251541
-	const MAIN_MENU_MUSIC = 3399698792
-	const SIGN_ON = 1295442366
-	const HORSE_JUMP = 1466783479
-	const SIGN_OFF = 1130713572
-	const DYNAMITE_EXPLOSION = 1512346300
-	const GAMEPLAY_MUSIC = 2322231365
-	const SFX_TEST = 3018450665
-	const HORSE_NEIGH_BABY_SITS = 3018083661
-	const HOG_SLEEPING = 998140643
-	const END_MUSIC = 1428057472
-	const DYNAMITE_ACTIVATED = 704518912
 	const BUTTON_DEACTIVATED = 3236117730
+	const BABY_TALK_THROW = 3342570235
 	const FIRE = 2678880713
+	const HORSE_NEIGH_POSITIVE = 487887902
 	const INTRO_MUSIC = 1539411869
+	const LASSO_THROW = 362990204
+	const HOG_WALK = 1493740447
+	const FRUIT_PLUCK = 336049027
+	const BABY_FLY = 1288370271
+	const HORSE_JUMP = 1466783479
+	const LEVEL_FINISH = 4278251541
+	const HOG_SLEEPING = 998140643
+	const MAIN_MENU_MUSIC = 3399698792
+	const SFX_TEST = 3018450665
+	const SIGN_ON = 1295442366
+	const GAMEPLAY_MUSIC = 2322231365
+	const SIGN_OFF = 1130713572
+	const LASSO_CATCH = 1367699361
+	const HORSE_KICK = 3198588461
+	const WHIP_TANSITION = 3944649217
+	const HORSE_FOOTSTEPS = 701459736
+	const LASSO_WHIRL = 2401031616
+	const END_MUSIC = 1428057472
+	const ROCK_MOVING = 2491012383
+	const BUTTON_ACTIVATED = 3746142581
+	const DYNAMITE_EXPLOSION = 1512346300
+	const DYNAMITE_ACTIVATED = 704518912
 
 	const _dict = {
-		"Lasso_Catch": LASSO_CATCH,
-		"Lasso_Throw": LASSO_THROW,
-		"Baby_Fly": BABY_FLY,
-		"Lasso_Whirl": LASSO_WHIRL,
-		"Hog_Walk": HOG_WALK,
-		"Horse_Footsteps": HORSE_FOOTSTEPS,
-		"Whip_Tansition": WHIP_TANSITION,
-		"Baby_Talk_Throw": BABY_TALK_THROW,
-		"Fruit_Pluck": FRUIT_PLUCK,
-		"Button_Activated": BUTTON_ACTIVATED,
-		"Rock_Moving": ROCK_MOVING,
-		"Horse_Kick": HORSE_KICK,
+		"Horse_Neigh_Baby_Sits": HORSE_NEIGH_BABY_SITS,
 		"Baby_Talk_Positive": BABY_TALK_POSITIVE,
 		"Hog_Eat": HOG_EAT,
-		"Horse_Neigh_Positive": HORSE_NEIGH_POSITIVE,
-		"Level_Finish": LEVEL_FINISH,
-		"Main_Menu_Music": MAIN_MENU_MUSIC,
-		"Sign_On": SIGN_ON,
-		"Horse_Jump": HORSE_JUMP,
-		"Sign_Off": SIGN_OFF,
-		"Dynamite_Explosion": DYNAMITE_EXPLOSION,
-		"Gameplay_Music": GAMEPLAY_MUSIC,
-		"SFX_TEST": SFX_TEST,
-		"Horse_Neigh_Baby_Sits": HORSE_NEIGH_BABY_SITS,
-		"Hog_Sleeping": HOG_SLEEPING,
-		"End_Music": END_MUSIC,
-		"Dynamite_Activated": DYNAMITE_ACTIVATED,
 		"Button_Deactivated": BUTTON_DEACTIVATED,
+		"Baby_Talk_Throw": BABY_TALK_THROW,
 		"Fire": FIRE,
-		"Intro_Music": INTRO_MUSIC
+		"Horse_Neigh_Positive": HORSE_NEIGH_POSITIVE,
+		"Intro_Music": INTRO_MUSIC,
+		"Lasso_Throw": LASSO_THROW,
+		"Hog_Walk": HOG_WALK,
+		"Fruit_Pluck": FRUIT_PLUCK,
+		"Baby_Fly": BABY_FLY,
+		"Horse_Jump": HORSE_JUMP,
+		"Level_Finish": LEVEL_FINISH,
+		"Hog_Sleeping": HOG_SLEEPING,
+		"Main_Menu_Music": MAIN_MENU_MUSIC,
+		"SFX_TEST": SFX_TEST,
+		"Sign_On": SIGN_ON,
+		"Gameplay_Music": GAMEPLAY_MUSIC,
+		"Sign_Off": SIGN_OFF,
+		"Lasso_Catch": LASSO_CATCH,
+		"Horse_Kick": HORSE_KICK,
+		"Whip_Tansition": WHIP_TANSITION,
+		"Horse_Footsteps": HORSE_FOOTSTEPS,
+		"Lasso_Whirl": LASSO_WHIRL,
+		"End_Music": END_MUSIC,
+		"Rock_Moving": ROCK_MOVING,
+		"Button_Activated": BUTTON_ACTIVATED,
+		"Dynamite_Explosion": DYNAMITE_EXPLOSION,
+		"Dynamite_Activated": DYNAMITE_ACTIVATED
 	}
 
 class STATES:
@@ -106,12 +106,12 @@ class AUX_BUSSES:
 
 class AUDIO_DEVICES:
 
-	const NO_OUTPUT = 2317455096
 	const SYSTEM = 3859886410
+	const NO_OUTPUT = 2317455096
 
 	const _dict = {
-		"No_Output": NO_OUTPUT,
-		"System": SYSTEM
+		"System": SYSTEM,
+		"No_Output": NO_OUTPUT
 	}
 
 class EXTERNAL_SOURCES:
